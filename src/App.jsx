7196d17ac3af1fdb0a1ea81eda8e3aee7a52bee7
Ghodke <mrunal.ghodke@accenture.com>
2025-06-28 22:21:46 +0530

    by default all articles is selected on home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,68 @@
-import './App.css'
-import Header from './Header'
-import SideNav from './SideNav'
-import HomePage from './HomePage'
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
-import BlogPage from './BlogPage';
-import React, { useState } from 'react';
-import Footer from './Footer';
+import { BrowserRouter as Router, Routes, Route, Navigate, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import './App.css';
 import BlogItem from './BlogItem';
+import BlogPage from './BlogPage';
 import FindRecipe from './FindRecipe';
+import Footer from './Footer';
+import Header from './Header';
+import HomePage from './HomePage';
+import SideNav from './SideNav';
 
-function App() {
-
-  const [clickedSideNavItem, setClickedSideNavItem] = useState( {
+function AppContent() {
+  const DEFAULT_NAV_ITEM = {
     id: 1,
     title: "ALL ARTICLES",
-  });
+  };
 
-  const handleSideNavClick=(item)=>{
-    setClickedSideNavItem({...item});
-  }
+  const [clickedSideNavItem, setClickedSideNavItem] = useState(DEFAULT_NAV_ITEM);
+  const location = useLocation(); 
+
+  useEffect(() => {
+    if (location.pathname === '/blog') {
+      setClickedSideNavItem(DEFAULT_NAV_ITEM);
+    }
+  }, [location.pathname]);
+
+  const handleSideNavClick = (item) => {
+    setClickedSideNavItem({ ...item });
+  };
 
   return (
     <>
-      <Router>
-        <Header />
-        <Routes>
-          <Route
-            path="/blog"
-            element={
-              <>
-                <HomePage />
-                <div className='d-flex main-wrapper'>
-                  <div className='side-nav-component'>
-                    <SideNav onItemClick={handleSideNavClick}/>
-                  </div>
-                  <div className='blog-page-component'>
-                    <BlogPage clickedSideNavItem={clickedSideNavItem} key={clickedSideNavItem.id}/>
-                  </div>
+      <Header />
+      <Routes>
+        <Route
+          path="/blog"
+          element={
+            <>
+              <HomePage />
+              <div className='d-flex main-wrapper'>
+                <div className='side-nav-component'>
+                  <SideNav onItemClick={handleSideNavClick} />
                 </div>
-              </>
-            }
-          />
-          <Route path="*" element={<Navigate to="/blog" />} />
-          <Route path="/blog/:id" element={<BlogItem />} />
-          <Route path="/findRecipe" element={<FindRecipe />} />
-        </Routes>
-        <Footer/>
-      </Router>
+                <div className='blog-page-component'>
+                  <BlogPage clickedSideNavItem={clickedSideNavItem} key={clickedSideNavItem.id} />
+                </div>
+              </div>
+            </>
+          }
+        />
+        <Route path="*" element={<Navigate to="/blog" />} />
+        <Route path="/blog/:id" element={<BlogItem />} />
+        <Route path="/findRecipe" element={<FindRecipe />} />
+      </Routes>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Router>
+      <AppContent /> 
+    </Router>
+  );
+}
+
+export default App;
