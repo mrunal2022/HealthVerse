fdb287c9c211a275ab7532566da01e875c19ffa4
Ghodke <mrunal.ghodke@accenture.com>
2025-06-28 12:20:36 +0530

    added selected side nav highlight

--- src/SideNav.jsx
+++ src/SideNav.jsx
@@ -1,22 +1,34 @@
-import React from 'react'
+import { useState } from 'react';
 import './SideNav.scss';
 import SideNavItems from './constants/HealthVerse.constants.js';
+
 const SideNav = ({ onItemClick }) => {
+  const [sideNavItems, setSideNavItems] = useState(
+    SideNavItems.map((item, index) => ({ ...item, selected: index === 0 }))
+  );
 
   const onClickSideNavItem = (item) => {
     onItemClick(item);
+
+    const updatedItem = sideNavItems.map((navItem) => ({
+      ...navItem,
+      selected: navItem.id === item.id,
+    }));
+    setSideNavItems(updatedItem);
   }
 
   return (
     <div>
       <div className='side-nav-container'>
-        {SideNavItems.map((item) => (
+        {sideNavItems.map((item) => (
           <div
-            className='side-nav-item roboto-font'
+            key={item.id}
+            className={`side-nav-item roboto-font ${item.selected ? 'selected-nav-item' : ''}`}
             onClick={() => onClickSideNavItem(item)}
           >
             {item.title}
-          </div>))}
+          </div>
+        ))}
       </div>
     </div>
   )
